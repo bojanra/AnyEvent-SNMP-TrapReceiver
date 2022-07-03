01fe45cc9f1448025f828df7ad9c5c8f177e5dd7
Bojan Ramšak <bojan.ramsak@rtvslo.si>
2022-07-03 23:14:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+AnyEvent::SNMP::TrapReceiver - It's new $module
+
+# SYNOPSIS
+
+    use AnyEvent::SNMP::TrapReceiver;
+
+# DESCRIPTION
+
+AnyEvent::SNMP::TrapReceiver is ...
+
+# LICENSE
+
+Copyright (C) Bojan Ramšak.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Bojan Ramšak <bojanr@gmx.net>
---
+++ lib/AnyEvent/SNMP/TrapReceiver.pm
@@ -0,0 +1,39 @@
+package AnyEvent::SNMP::TrapReceiver;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+AnyEvent::SNMP::TrapReceiver - It's new $module
+
+=head1 SYNOPSIS
+
+    use AnyEvent::SNMP::TrapReceiver;
+
+=head1 DESCRIPTION
+
+AnyEvent::SNMP::TrapReceiver is ...
+
+=head1 LICENSE
+
+Copyright (C) Bojan Ramšak.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Bojan Ramšak E<lt>bojanr@gmx.netE<gt>
+
+=cut
+
